1c9ceef3a5798b4bf18c5bc8de39e2e08294eb98
Nikita Fedyashev <nfedyashev@gmail.com>
2012-11-05 07:44:14 +0100

    Do not use pre versions because it is too much for TimecopConsole

--- lib/timecop_console/version.rb
+++ lib/timecop_console/version.rb
@@ -3,13 +3,12 @@ module TimecopConsole
     MAJOR = 0 unless defined? MAJOR
     MINOR = 1 unless defined? MINOR
     PATCH = 0 unless defined? PATCH
-    PRE = nil unless defined? PRE
 
     class << self
 
       # @return [String]
       def to_s
-        [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+        [MAJOR, MINOR, PATCH].compact.join('.')
       end
 
     end
