61942712e8a6a6e1727dda3180d482206391acf2
Nikita Fedyashev <nfedyashev@gmail.com>
2012-11-05 07:42:03 +0100

    Add Timecop ControllerMethods specs

---
+++ spec/controllers/sample_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe SampleController do
+  describe "GET to :index" do
+    controller do
+      def index
+        render :nothing => true
+      end
+    end
+
+    it 'calls around filter method' do
+      controller.should_receive(:handle_timecop_offset)
+
+      get :index
+    end
+  end
+
+  describe "#handle_timecop_offset" do
+    controller do
+      def index
+        raise Time.zone.now.to_s
+      end
+    end
+
+    it 'sets proper time inside action method' do
+      frozen_time = 1.year.from_now
+
+      session[TimecopConsole::SESSION_KEY_NAME] = frozen_time
+
+      expect { get :index }.to raise_error(frozen_time.to_s)
+    end 
+  end
+end
---
+++ spec/dummy_app/app/controllers/sample_controller.rb
@@ -0,0 +1,2 @@
+class SampleController < ApplicationController
+end
