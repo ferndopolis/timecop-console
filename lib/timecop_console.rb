b8f38829c92dbd4883944c49beab7d166c174476
Nikita Fedyashev <nfedyashev@gmail.com>
2012-11-05 07:19:54 +0100

    Properly namespace controller methods module

--- lib/timecop_console.rb
+++ lib/timecop_console.rb
@@ -1,8 +1,8 @@
 require 'timecop_console/engine'
-require 'timecop_console/timecop_controller_methods'
+require 'timecop_console/controller_methods'
 
 module TimecopConsole
   SESSION_KEY_NAME = :timecop_adjusted_time unless defined?(SESSION_KEY_NAME)
 end
 
-ActionController::Base.send(:include, TimecopControllerMethods)
+ActionController::Base.send(:include, TimecopConsole::ControllerMethods)
---
+++ lib/timecop_console/controller_methods.rb
@@ -0,0 +1,34 @@
+# Defines extensions applied to ActionController::Base to support our time travel
+module TimecopConsole
+  module ControllerMethods
+    def self.included(base)
+      base.class_eval do
+        around_filter :handle_timecop_offset
+      end
+    end
+
+    # to be used as an around_filter
+    def handle_timecop_offset
+      # Establish now
+      if session[TimecopConsole::SESSION_KEY_NAME].present?
+        Rails.logger.debug "[timecop-console] Time traveling to #{session[TimecopConsole::SESSION_KEY_NAME].to_s}"
+        Timecop.travel(session[TimecopConsole::SESSION_KEY_NAME])
+      else
+        Timecop.return
+      end
+
+      # Run the intended action
+      yield
+
+      if session[TimecopConsole::SESSION_KEY_NAME].present?
+        # we want to continue to slide time forward, even if it's only 3 seconds at a time.
+        # this ensures that subsequent calls during the same "time travel" actually pass time
+        adjusted_time = Time.now + 3
+        Rails.logger.debug "[timecop-console] Resetting session to: #{adjusted_time}"
+        session[TimecopConsole::SESSION_KEY_NAME] = adjusted_time
+      end
+    end
+
+    private :handle_timecop_offset
+  end
+end
