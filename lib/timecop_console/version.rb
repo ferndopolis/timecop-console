764a53515b90113767876db92db5561ec0a5ea13
Nikita Fedyashev <nfedyashev@gmail.com>
2012-11-14 20:16:06 +0100

    Bump timecop-console version

--- lib/timecop_console/version.rb
+++ lib/timecop_console/version.rb
@@ -1,7 +1,7 @@
 module TimecopConsole
   class Version
     MAJOR = 0 unless defined? MAJOR
-    MINOR = 1 unless defined? MINOR
+    MINOR = 2 unless defined? MINOR
     PATCH = 0 unless defined? PATCH
 
     class << self
