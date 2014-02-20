f394b8c04c4068a6aed9274901f83ee0b8614c16
Fernando Marcheselli <fmarcheselli@gmail.com>
2014-02-20 12:12:33 -0800

    add timecop_link_to helper method
    Users can add specific dates link to their views by passing a Date
    object to timecop_link_to.

---
+++ app/helpers/timecop_console/main_helper.rb
@@ -0,0 +1,12 @@
+module TimecopConsole
+  module MainHelper
+    def timecop_link_to(date)
+      link_to date.strftime("%B %d, %Y"), timecop_console.update_path(timecop:
+            {
+              'current_time(1i)' => date.year, 'current_time(2i)' => date.month,
+              'current_time(3i)' => date.day, 'current_time(4i)' => 12,
+              'current_time(5i)' => 0
+            }), method: :post
+    end
+  end
+end
