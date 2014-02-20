3a53e5b9335d61ccc874d64521a67faa87337fb4
Fernando Marcheselli <fmarcheselli@gmail.com>
2014-02-20 12:18:12 -0800

    MainHelper included in client's application helper

--- lib/timecop_console/engine.rb
+++ lib/timecop_console/engine.rb
@@ -5,5 +5,8 @@ require 'timecop_console'
 module TimecopConsole
   class Engine < Rails::Engine
     isolate_namespace TimecopConsole
+    config.to_prepare do
+      ApplicationController.helper(MainHelper)
+    end
   end
 end
