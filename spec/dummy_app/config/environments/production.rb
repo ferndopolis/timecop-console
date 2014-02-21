7dba07305f975339569ab4bd948e79a9176a0322
Fernando Marcheselli <fmarcheselli@gmail.com>
2014-02-20 19:19:14 -0800

    set eager_load for dummy app
    recommend by rails4

--- spec/dummy_app/config/environments/production.rb
+++ spec/dummy_app/config/environments/production.rb
@@ -48,4 +48,5 @@ DummyApp::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.eager_load = true
 end
--- spec/dummy_app/config/environments/test.rb
+++ spec/dummy_app/config/environments/test.rb
@@ -29,4 +29,6 @@ DummyApp::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.eager_load = false
 end
