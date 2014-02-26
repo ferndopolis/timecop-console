9883800c1b37a0a46d0f337027218f44aca1a5f0
Michael Kirk <michael.john.kirk@gmail.com>
2014-02-26 15:28:50 -0800

    use button_to instead of link_to

--- app/helpers/timecop_console/main_helper.rb
+++ app/helpers/timecop_console/main_helper.rb
@@ -5,14 +5,17 @@ module TimecopConsole
         raise ArgumentError, "Argument must be a Date object"
       end
 
-      link_to date.strftime("%B %d, %Y"), timecop_console.update_path(timecop:
-            {
-              'current_time(1i)' => date.year,
-              'current_time(2i)' => date.month,
-              'current_time(3i)' => date.day,
-              'current_time(4i)' => 12,
-              'current_time(5i)' => 0
-            }), method: :post
+      update_path = timecop_console.update_path(timecop: {
+        'current_time(1i)' => date.year,
+        'current_time(2i)' => date.month,
+        'current_time(3i)' => date.day,
+        'current_time(4i)' => 12,
+        'current_time(5i)' => 0
+      })
+
+      button_to(date.strftime("%B %d, %Y"),
+                update_path,
+                method: :post)
     end
   end
 end
--- spec/helpers/main_helper_spec.rb
+++ spec/helpers/main_helper_spec.rb
@@ -6,7 +6,7 @@ describe TimecopConsole::MainHelper do
       let(:date) { Date.parse('Dec 12, 1950') }
 
       it 'should return a link with timecop console route' do
-        helper.time_travel_to(date).should == "<a data-method=\"post\" href=\"/timecop_console/update?timecop%5Bcurrent_time%281i%29%5D=1950&amp;timecop%5Bcurrent_time%282i%29%5D=12&amp;timecop%5Bcurrent_time%283i%29%5D=12&amp;timecop%5Bcurrent_time%284i%29%5D=12&amp;timecop%5Bcurrent_time%285i%29%5D=0\" rel=\"nofollow\">December 12, 1950</a>"
+        helper.time_travel_to(date).should == "<form action=\"/timecop_console/update?timecop%5Bcurrent_time%281i%29%5D=1950&amp;timecop%5Bcurrent_time%282i%29%5D=12&amp;timecop%5Bcurrent_time%283i%29%5D=12&amp;timecop%5Bcurrent_time%284i%29%5D=12&amp;timecop%5Bcurrent_time%285i%29%5D=0\" class=\"button_to\" method=\"post\"><div><input type=\"submit\" value=\"December 12, 1950\" /></div></form>"
       end
     end
     context "when a string is passed" do
